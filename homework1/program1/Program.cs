50f5f7ac2c5b6c33bc09a213d8b46612baadb129
DaiDaotang <1025563447@qq.com>
2018-09-10 22:27:41 +0800

    first homework

---
+++ homework1/program1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = " ";
+            string t = " ";
+            double a, b;
+            Console.Write("Please input the first double:");
+            s = Console.ReadLine();
+            Console.Write("Please input the second double:");
+            t = Console.ReadLine();
+            a = Double.Parse(s);
+            b = Double.Parse(t);
+            Console.WriteLine("乘积是"+ a * b);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ homework1/program2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string s = " ";
+        string t = " ";
+        double a, b;
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            s = textBox1.Text;
+            a = Double.Parse(s);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.textBox3.Text = "乘积是" + a * b; 
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            t = textBox2.Text;
+            b = Double.Parse(t);
+        }
+    }
+}
