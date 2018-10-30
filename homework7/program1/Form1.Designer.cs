6cd9705a047fb5674582f4e72a6f9bc14fc5ccf2
DaiDaotang <1025563447@qq.com>
2018-10-30 20:57:10 +0800

    winform

---
+++ homework7/program1/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace program1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.change = new System.Windows.Forms.Button();
+            this.check = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Client = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Product = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Quantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.remove = new System.Windows.Forms.Button();
+            this.idDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.orderbinding = new System.Windows.Forms.BindingSource(this.components);
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orderbinding)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.remove);
+            this.panel1.Controls.Add(this.change);
+            this.panel1.Controls.Add(this.check);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(903, 60);
+            this.panel1.TabIndex = 0;
+            // 
+            // change
+            // 
+            this.change.Location = new System.Drawing.Point(301, 6);
+            this.change.Name = "change";
+            this.change.Size = new System.Drawing.Size(75, 36);
+            this.change.TabIndex = 3;
+            this.change.Text = "修改";
+            this.change.UseVisualStyleBackColor = true;
+            this.change.Click += new System.EventHandler(this.change_Click);
+            // 
+            // check
+            // 
+            this.check.Location = new System.Drawing.Point(206, 6);
+            this.check.Name = "check";
+            this.check.Size = new System.Drawing.Size(75, 36);
+            this.check.TabIndex = 2;
+            this.check.Text = "查询";
+            this.check.UseVisualStyleBackColor = true;
+            this.check.Click += new System.EventHandler(this.check_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(100, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "产品Id",
+            "用户名字",
+            "产品名字"});
+            this.comboBox1.Location = new System.Drawing.Point(3, 14);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(91, 23);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.Client});
+            this.dataGridView1.DataSource = this.orderbinding;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 66);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(331, 246);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Client
+            // 
+            this.Client.DataPropertyName = "Client";
+            this.Client.HeaderText = "Client";
+            this.Client.Name = "Client";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoGenerateColumns = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn1,
+            this.Product,
+            this.Price,
+            this.Quantity});
+            this.dataGridView2.DataMember = "Details";
+            this.dataGridView2.DataSource = this.orderbinding;
+            this.dataGridView2.Location = new System.Drawing.Point(337, 66);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 27;
+            this.dataGridView2.Size = new System.Drawing.Size(566, 246);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // Product
+            // 
+            this.Product.DataPropertyName = "Product";
+            this.Product.HeaderText = "Product";
+            this.Product.Name = "Product";
+            // 
+            // Price
+            // 
+            this.Price.DataPropertyName = "Price";
+            this.Price.HeaderText = "Price";
+            this.Price.Name = "Price";
+            // 
+            // Quantity
+            // 
+            this.Quantity.DataPropertyName = "Quantity";
+            this.Quantity.HeaderText = "Quantity";
+            this.Quantity.Name = "Quantity";
+            // 
+            // remove
+            // 
+            this.remove.Location = new System.Drawing.Point(400, 6);
+            this.remove.Name = "remove";
+            this.remove.Size = new System.Drawing.Size(75, 35);
+            this.remove.TabIndex = 4;
+            this.remove.Text = "删除";
+            this.remove.UseVisualStyleBackColor = true;
+            this.remove.Click += new System.EventHandler(this.remove_Click);
+            // 
+            // idDataGridViewTextBoxColumn1
+            // 
+            this.idDataGridViewTextBoxColumn1.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn1.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn1.Name = "idDataGridViewTextBoxColumn1";
+            // 
+            // orderbinding
+            // 
+            this.orderbinding.DataSource = typeof(program.Order);
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 450);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orderbinding)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button check;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.BindingSource orderbinding;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Client;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Product;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Quantity;
+        private System.Windows.Forms.Button change;
+        private System.Windows.Forms.Button remove;
+    }
+}
+
---
+++ homework7/program1/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using program;
+
+namespace program1
+{
+    public partial class Form1 : Form
+    {
+        //OrderService service = new OrderService();
+
+        OrderService service = OrderService.Import ("s.xml");
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            //OrderDetail orderDetails1 = new OrderDetail("1", "apple", 3, 2);
+            //Order order1 = new Order("1", "customer1");
+            //order1.AddDetails(orderDetails1);
+            //OrderDetail orderDetails2 = new OrderDetail("2", "orange", 2, 1);
+            //Order order2 = new Order("2", "customer2");
+            //order2.AddDetails(orderDetails2);
+            //service.AddOrder(order1);
+            //service.AddOrder(order2);
+
+            orderbinding.DataSource = service.orderList;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void check_Click(object sender, EventArgs e)
+        {
+            if(comboBox1 .SelectedItem .ToString ()=="产品Id")
+            {
+                orderbinding.DataSource = service.CheckById(textBox1.Text);
+            }
+            else if(comboBox1.SelectedItem.ToString() == "用户名字")
+            {
+                orderbinding.DataSource = service.CheckByClient(textBox1.Text);
+            }
+            else if(comboBox1.SelectedItem.ToString() == "产品名字")
+            {
+                orderbinding.DataSource = service.CheckByProduct(textBox1.Text);
+            }
+        }
+
+        private void change_Click(object sender, EventArgs e)
+        {
+            
+            service.Export("s.xml");
+        }
+
+        private void remove_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.ToString() == "产品Id")
+            {
+                service.DeleteById(textBox1.Text);
+                orderbinding.DataSource = service.orderList;
+            }
+            else if (comboBox1.SelectedItem.ToString() == "用户名字")
+            {
+                service.DeleteByClient(textBox1.Text);
+            }
+            service.Export("s.xml");
+            //else if (comboBox1.SelectedItem.ToString() == "产品名字")
+            //{
+            //    orderbinding.DataSource = service.CheckByProduct(textBox1.Text);
+            //}
+        }
+    }
+}
